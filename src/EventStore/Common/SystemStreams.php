c0b8473042c45a75d2283219e55f4d30e82a2df8
prolic <saschaprolic@googlemail.com>
2018-03-30 23:01:28 +0800

    add system common consts

---
+++ src/EventStore/Common/SystemStreams.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Prooph\EventStore\Common;
+
+class SystemStreams
+{
+    public const StreamsStream = '$streams';
+    public const SettingsStream = '$settings';
+    public const StatsStreamPrefix = '$stats';
+
+    public static function metastramOf(string $streamId): string
+    {
+        return '$$' . $streamId;
+    }
+
+    public static function isMetastream(string $streamId): bool
+    {
+        return substr($streamId, 0, 2) === '$$';
+    }
+
+    public static function originalStreamOf(string $metastreamId): string
+    {
+        return substr($metastreamId, 2);
+    }
+}
