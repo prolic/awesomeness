928b73e8e6c7e8facfee117686894b2b5946ecfb
prolic <saschaprolic@googlemail.com>
2018-04-08 22:47:37 +0800

    implement stats http api

--- src/EventStoreHttpClient/UserManagement/ClientOperations/GetAllUsersOperation.php
+++ src/EventStoreHttpClient/UserManagement/ClientOperations/GetAllUsersOperation.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Prooph\EventStoreHttpClient\UserManagement\ClientOperations;
 
-use Http\Client\HttpAsyncClient;
-use Http\Message\RequestFactory;
-use Http\Message\UriFactory;
 use Prooph\EventStoreClient\Exception\AccessDenied;
 use Prooph\EventStoreClient\Task\GetAllUsersTask;
-use Prooph\EventStoreClient\UserCredentials;
 use Prooph\EventStoreClient\UserManagement\UserDetails;
 use Prooph\EventStoreHttpClient\ClientOperations\Operation;
 use Prooph\EventStoreHttpClient\Http\RequestMethod;
@@ -18,16 +14,6 @@ use Psr\Http\Message\ResponseInterface;
 /** @internal */
 class GetAllUsersOperation extends Operation
 {
-    public function __construct(
-        HttpAsyncClient $asyncClient,
-        RequestFactory $requestFactory,
-        UriFactory $uriFactory,
-        string $baseUri,
-        ?UserCredentials $userCredentials
-    ) {
-        parent::__construct($asyncClient, $requestFactory, $uriFactory, $baseUri, $userCredentials);
-    }
-
     public function task(): GetAllUsersTask
     {
         $request = $this->requestFactory->createRequest(
