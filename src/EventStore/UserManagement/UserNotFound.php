d15448ea0987d38026c198a232600143d10f36b7
prolic <saschaprolic@googlemail.com>
2018-04-22 22:16:07 +0800

    change base exception

--- src/EventStore/UserManagement/UserNotFound.php
+++ src/EventStore/UserManagement/UserNotFound.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Prooph\EventStore\UserManagement;
 
+use Prooph\EventStore\Exception\RuntimeException;
+
 /** @internal */
-class UserNotFound extends \RuntimeException
+class UserNotFound extends RuntimeException
 {
     public static function withLogin(string $login): UserNotFound
     {
--- src/HttpEventStore/ProjectionManagement/ProjectionNotFound.php
+++ src/HttpEventStore/ProjectionManagement/ProjectionNotFound.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Prooph\HttpEventStore\ProjectionManagement;
 
+use Prooph\EventStore\Exception\RuntimeException;
+
 /** @internal */
-class ProjectionNotFound extends \RuntimeException
+class ProjectionNotFound extends RuntimeException
 {
 }
