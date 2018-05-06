86f4006136d7b882da037d818ae04ed854d75f20
prolic <saschaprolic@googlemail.com>
2018-05-06 23:46:35 +0800

    implement updateConfig + updateQuery for pdo projections

--- src/PdoEventStore/ProjectionManagement/PdoProjectionManagement.php
+++ src/PdoEventStore/ProjectionManagement/PdoProjectionManagement.php
@@ -16,6 +16,7 @@ use Prooph\EventStore\ProjectionManagement\ProjectionConfig;
 use Prooph\EventStore\ProjectionManagement\ProjectionDefinition;
 use Prooph\EventStore\ProjectionManagement\ProjectionDetails;
 use Prooph\EventStore\ProjectionManagement\ProjectionManagement;
+use Prooph\EventStore\ProjectionManagement\ProjectionNotFound;
 use Prooph\EventStore\Projections\ProjectionEventTypes;
 use Prooph\EventStore\Projections\ProjectionNames;
 use Prooph\EventStore\Projections\StandardProjections;
@@ -226,7 +227,48 @@ final class PdoProjectionManagement implements ProjectionManagement
 
     public function updateConfig(string $name, ProjectionConfig $config, UserCredentials $userCredentials = null): void
     {
-        // TODO: Implement updateConfig() method.
+        if (StandardProjections::isStandardProjection($name)) {
+            throw new ProjectionException('Cannot override standard projections');
+        }
+
+        $projectionId = $this->fetchProjectionId($name);
+
+        $this->pdoEventStoreConnection->appendToStream(
+            ProjectionNames::ProjectionsMasterStream,
+            ExpectedVersion::Any,
+            [
+                new EventData(
+                    EventId::generate(),
+                    '$prepared',
+                    true,
+                    json_encode([
+                        'id' => $projectionId,
+                    ]),
+                    ''
+                ),
+            ],
+            $userCredentials
+        );
+
+        $streamName = ProjectionNames::ProjectionsStreamPrefix . $name;
+
+        $data = $this->fetchLastProjectionStreamDataByEventType($streamName, ProjectionEventTypes::ProjectionUpdated);
+        $data = array_merge($data, $config->toArray());
+
+        $this->pdoEventStoreConnection->appendToStream(
+            ProjectionNames::ProjectionsStreamPrefix . $name,
+            ExpectedVersion::Any,
+            [
+                new EventData(
+                    EventId::generate(),
+                    ProjectionEventTypes::ProjectionUpdated,
+                    true,
+                    json_encode($data),
+                    ''
+                ),
+            ],
+            $userCredentials
+        );
     }
 
     public function updateQuery(
@@ -236,7 +278,52 @@ final class PdoProjectionManagement implements ProjectionManagement
         bool $emitEnabled,
         UserCredentials $userCredentials = null
     ): void {
-        // TODO: Implement updateQuery() method.
+        if ($type !== 'PHP') {
+            throw new ProjectionException('Only projection type support for now is \'PHP\'');
+        }
+
+        if (StandardProjections::isStandardProjection($name)) {
+            throw new ProjectionException('Cannot override standard projections');
+        }
+
+        $projectionId = $this->fetchProjectionId($name);
+
+        $this->pdoEventStoreConnection->appendToStream(
+            ProjectionNames::ProjectionsMasterStream,
+            ExpectedVersion::Any,
+            [
+                new EventData(
+                    EventId::generate(),
+                    '$prepared',
+                    true,
+                    json_encode([
+                        'id' => $projectionId,
+                    ]),
+                    ''
+                ),
+            ],
+            $userCredentials
+        );
+
+        $streamName = ProjectionNames::ProjectionsStreamPrefix . $name;
+
+        $data = $this->fetchLastProjectionStreamDataByEventType($streamName, ProjectionEventTypes::ProjectionUpdated);
+        $data['query'] = $query;
+
+        $this->pdoEventStoreConnection->appendToStream(
+            ProjectionNames::ProjectionsStreamPrefix . $name,
+            ExpectedVersion::Any,
+            [
+                new EventData(
+                    EventId::generate(),
+                    ProjectionEventTypes::ProjectionUpdated,
+                    true,
+                    json_encode($data),
+                    ''
+                ),
+            ],
+            $userCredentials
+        );
     }
 
     private function defaultProjectionConfig(): array
@@ -254,10 +341,10 @@ final class PdoProjectionManagement implements ProjectionManagement
         string $name,
         string $type,
         string $query,
-        ?bool $enabled,
+        bool $enabled,
         ?bool $checkpoints,
         ?bool $emit,
-        bool $trackEmittedStreams,
+        ?bool $trackEmittedStreams,
         UserCredentials $userCredentials = null
     ): CreateProjectionResult {
         if ($type !== 'PHP') {
@@ -370,5 +457,49 @@ final class PdoProjectionManagement implements ProjectionManagement
         );
 
         return CreateProjectionResult::success();
+    }
+
+    private function fetchProjectionId(string $name): string
+    {
+        $statement = $this->connection->prepare('SELECT projection_id FROM projections WHERE projection_name = ?;');
+        $statement->execute([$name]);
+
+        if ($statement->rowCount() === 0) {
+            throw ProjectionNotFound::withName($name);
+        }
+
+        $statement->setFetchMode(PDO::FETCH_OBJ);
+
+        return $statement->fetch()->projection_id;
+    }
+
+    private function fetchLastProjectionStreamDataByEventType(string $name, string $type): array
+    {
+        $sql = <<<SQL
+SELECT
+    e2.event_id as event_id,
+    e1.event_number as event_number,
+    COALESCE(e1.event_type, e2.event_type) as event_type,
+    COALESCE(e1.data, e2.data) as data,
+    COALESCE(e1.meta_data, e2.meta_data) as meta_data,
+    COALESCE(e1.is_json, e2.is_json) as is_json,
+    COALESCE(e1.updated, e2.updated) as updated
+FROM
+    events e1
+LEFT JOIN events e2
+    ON (e1.link_to = e2.eventId)
+LEFT JOIN streams
+    ON streams.stream_id = e1.stream_id
+WHERE streams.name = ?
+AND e1.event_type >= ?
+ORDER BY e1.event_number DESC
+LIMIT ?
+SQL;
+
+        $statement = $this->connection->prepare($sql);
+        $statement->execute([$name, $type]);
+        $statement->setFetchMode(PDO::FETCH_OBJ);
+
+        return json_decode($statement->fetch()->data, true);
     }
 }
