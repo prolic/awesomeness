6e8133d3ebc62fddaa27c97465f813f23e420a26
prolic <saschaprolic@googlemail.com>
2018-05-11 23:11:42 +0800

    add default projections to db.sql

--- src/PdoEventStore/scripts/postgres/event-store-db.sql
+++ src/PdoEventStore/scripts/postgres/event-store-db.sql
@@ -58,11 +58,21 @@ INSERT INTO users (username, full_name, password_hash, disabled) VALUES
 INSERT INTO user_roles (rolename, username) VALUES
     ('$admins', 'admin'),
     ('$ops', 'ops');
+INSERT INTO streams (stream_id, stream_name, deleted, mark_deleted) VALUES
+    ('eb2686af-a164-4fd4-9de1-1c830abab796', '$projections-$streams', false, false),
+    ('f385823c-3a15-4bf5-b775-540c9f359e20', '$projections-$stream_by_category', false, false),
+    ('17818667-726e-4815-9d14-968c88e85fcd', '$projections-$by_category', false, false),
+    ('27ede519-046e-472b-ada7-497af013a06f', '$projections-$by_event_type', false, false);
 INSERT INTO projections (projection_name, projection_id) VALUES
     ('$streams', '2eca01afe77e497b82cd60d8804bcd7d'),
     ('$stream_by_category', '8128b078465d493b84ebf643bce6c0f5'),
     ('$by_category', '3ef042a59e4747898955b39db8c5e4a1'),
     ('$by_event_type', '7cf319276a8340e9abafa4df7aa60e5c');
+INSERT INTO events (event_id, event_number, event_type, data, meta_data, stream_id, is_json, updated) VALUES
+    ('5b6f0887-0ff4-4e59-b185-0a61dee10489', 0, '$ProjectionUpdated', '{"handlerType":"PHP","query":"","mode":"Continous","sourceDefinition":{"allEvents":true,"allStreams":true,"categories":[],"events":[],"streams":[],"options":{"definedFold":true,"includeLinks":true}},"emitEnabled":true,"checkpointsDisabled":false,"trackEmittedStreams":false,"runAs":{"name":"$system"},"checkpointHandledThreshold":4000,"checkpointUnhandledBytesThreshold":10000000,"pendingEventsThreshold":5000,"maxWriteBatchLength":500}', '', 'eb2686af-a164-4fd4-9de1-1c830abab796', true, '2018-04-28T08:04:06.512675Z'),
+    ('62169efe-6826-4c90-8567-c08d66d1f7ee', 0, '$ProjectionUpdated', '{"handlerType":"PHP","query":"","mode":"Continous","sourceDefinition":{"allEvents":true,"allStreams":true,"categories":[],"events":[],"streams":[],"options":{"definedFold":true,"includeLinks":true}},"emitEnabled":true,"checkpointsDisabled":false,"trackEmittedStreams":false,"runAs":{"name":"$system"},"checkpointHandledThreshold":4000,"checkpointUnhandledBytesThreshold":10000000,"pendingEventsThreshold":5000,"maxWriteBatchLength":500}', '', 'f385823c-3a15-4bf5-b775-540c9f359e20', true, '2018-04-28T08:04:06.512675Z'),
+    ('0e76d92f-18ed-44db-89c8-a15fdff9070b', 0, '$ProjectionUpdated', '{"handlerType":"PHP","query":"","mode":"Continous","sourceDefinition":{"allEvents":true,"allStreams":true,"categories":[],"events":[],"streams":[],"options":{"definedFold":true,"includeLinks":true}},"emitEnabled":true,"checkpointsDisabled":false,"trackEmittedStreams":false,"runAs":{"name":"$system"},"checkpointHandledThreshold":4000,"checkpointUnhandledBytesThreshold":10000000,"pendingEventsThreshold":5000,"maxWriteBatchLength":500}', '', '17818667-726e-4815-9d14-968c88e85fcd', true, '2018-04-28T08:04:06.512675Z'),
+    ('32ba7716-50de-447e-ad99-e55182d5b80b', 0, '$ProjectionUpdated', '{"handlerType":"PHP","query":"","mode":"Continous","sourceDefinition":{"allEvents":true,"allStreams":true,"categories":[],"events":[],"streams":[],"options":{"definedFold":true,"includeLinks":true}},"emitEnabled":true,"checkpointsDisabled":false,"trackEmittedStreams":false,"runAs":{"name":"$system"},"checkpointHandledThreshold":4000,"checkpointUnhandledBytesThreshold":10000000,"pendingEventsThreshold":5000,"maxWriteBatchLength":500}', '', '27ede519-046e-472b-ada7-497af013a06f', true, '2018-04-28T08:04:06.512675Z');
 
 -- @todo write user stream acl into event-store
 -- @todo make this a script executed from php (setup_event_store.php)
