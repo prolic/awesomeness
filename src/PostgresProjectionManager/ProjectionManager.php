5ae98bfbcf6400e0047ee400be95852ea4f52855
prolic <saschaprolic@googlemail.com>
2018-06-17 15:34:39 +0800

    add load system settings operations

--- src/PostgresProjectionManager/ProjectionManager.php
+++ src/PostgresProjectionManager/ProjectionManager.php
@@ -22,6 +22,7 @@ use Prooph\EventStore\Common\SystemStreams;
 use Prooph\EventStore\Exception\ProjectionNotFound;
 use Prooph\EventStore\Exception\RuntimeException;
 use Prooph\EventStore\SystemSettings;
+use Prooph\PostgresProjectionManager\Operations\LoadSystemSettingsOperation;
 use Psr\Log\LoggerInterface as PsrLogger;
 use Throwable;
 use function Amp\call;
@@ -102,7 +103,7 @@ class ProjectionManager
                 );
             }
 
-            yield new Coroutine($this->loadSystemSettings());
+            $this->settings = yield new Coroutine((new LoadSystemSettingsOperation())($this->pool));
 
             /** @var ResultSet $result */
             $result = yield $this->pool->execute('SELECT projection_id, projection_name from projections;');
@@ -135,50 +136,6 @@ class ProjectionManager
             Loop::stop();
             yield new Failure($e);
         }
-    }
-
-    /** @throws Throwable */
-    private function loadSystemSettings(): Generator // @todo do we really need this here?
-    {
-        /** @var Statement $statement */
-        $statement = yield $this->pool->prepare(<<<SQL
-SELECT
-    COALESCE(e1.event_id, e2.event_id) as event_id,
-    e1.event_number as event_number,
-    COALESCE(e1.event_type, e2.event_type) as event_type,
-    COALESCE(e1.data, e2.data) as data,
-    COALESCE(e1.meta_data, e2.meta_data) as meta_data,
-    COALESCE(e1.is_json, e2.is_json) as is_json,
-    COALESCE(e1.updated, e2.updated) as updated
-FROM
-    events e1
-LEFT JOIN events e2
-    ON (e1.link_to_stream_name = e2.stream_name AND e1.link_to_event_number = e2.event_number)
-WHERE e1.stream_name = ?
-ORDER BY e1.event_number DESC
-LIMIT 1
-SQL
-        );
-        /** @var ResultSet $result */
-        $result = yield $statement->execute([SystemStreams::SettingsStream]);
-
-        if (! yield $result->advance(ResultSet::FETCH_OBJECT)) {
-            $this->settings = SystemSettings::default();
-
-            return null;
-        }
-
-        $event = $result->getCurrent();
-
-        $data = \json_decode($event->data, true);
-
-        if (0 !== \json_last_error()) {
-            throw new Error('Could not json decode system settings');
-        }
-
-        $this->settings = SystemSettings::fromArray($data);
-
-        return null;
     }
 
     /**
