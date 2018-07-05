5678efc648cc4df517b7c54febe66ab19993b425
prolic <saschaprolic@googlemail.com>
2018-07-05 18:08:48 +0800

    add raw and json event

---
+++ src/EventStore/JsonEvent.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Prooph\EventStore;
+
+/**
+ * Represents a json event to be written.
+ */
+class JsonEvent extends EventData
+{
+    public function __construct(?EventId $eventId, string $eventType, string $data = '', string $metaData = '')
+    {
+        parent::__construct($eventId, $eventType, true, $data, $metaData);
+    }
+}
